5906ca57ea86af9b0fd1297dd101167dddc5f228
Houcine MANZAT <Houcine.MANZAT.ext@nordnet.com>
2023-06-12 10:29:22 +0200

    first commit

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	log.Println(r.RemoteAddr, r.Method, r.URL.String())
+
+	fmt.Fprintf(w, "hello Mr manzat NEWAAAAA\n")
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	addr := ":" + port
+
+	log.Printf("listening on %s", addr)
+	log.Fatal(http.ListenAndServe(addr, nil))
+}
